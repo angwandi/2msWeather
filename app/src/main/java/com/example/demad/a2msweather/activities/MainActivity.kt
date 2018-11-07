5bc1a3ecfe9e21bc3bf7c07aa0bf128c54ab95ae
Angwandi <demsdema217@gmail.com>
2018-11-07 18:42:53 +0000

    add a recycler view adapter

--- app/src/main/java/com/example/demad/a2msweather/activities/MainActivity.kt
+++ app/src/main/java/com/example/demad/a2msweather/activities/MainActivity.kt
@@ -1,16 +1,17 @@
-package com.example.demad.a2msweather
+package com.example.demad.a2msweather.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.demad.a2msweather.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/demad/a2msweather/adapters/ForecastListAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.demad.a2msweather.adapters
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ForecastListAdapter(private val items: List<String>) :
+    RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
