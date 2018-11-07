b470c8036740b6835cdaeb2751c2d4198c556292
Angwandi <demsdema217@gmail.com>
2018-11-07 18:06:16 +0000

    Add recycler view

--- app/src/main/java/com/example/demad/a2msweather/MainActivity.kt
+++ app/src/main/java/com/example/demad/a2msweather/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.demad.a2msweather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
     }
 }
