9b27c7a710270f5a51dd5e27d98d888c128ff3d7
Angwandi <demsdema217@gmail.com>
2018-11-07 19:54:45 +0000

    add a line of anko

--- app/src/main/java/com/example/demad/a2msweather/activities/MainActivity.kt
+++ app/src/main/java/com/example/demad/a2msweather/activities/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.demad.a2msweather.activities
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.demad.a2msweather.R
+import com.example.demad.a2msweather.adapters.ForecastListAdapter
+import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+
+fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
 
 class MainActivity : AppCompatActivity() {
+    private val items = listOf(
+        "Mon 6/23 - Sunny - 31/17",
+        "Tue 6/24 - Foggy - 21/8",
+        "Wed 6/25 - Cloudy - 22/17",
+        "Thurs 6/26 - Rainy - 18/11",
+        "Fri 6/27 - Foggy - 21/10",
+        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+        "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
-        forecastList.layoutManager = LinearLayoutManager(this)
+        //using Anko
+        val forecastList: RecyclerView = find(R.id.forecast_list)
+//        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+        longToast("weather")
+        toast("fun")
     }
 }
