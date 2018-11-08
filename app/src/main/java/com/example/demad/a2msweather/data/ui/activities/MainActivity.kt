ee40ebbd910510edf19ef75c0350cfca0b1d7493
Angwandi <demsdema217@gmail.com>
2018-11-08 23:16:00 +0000

    add response class

--- app/src/main/java/com/example/demad/a2msweather/data/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/demad/a2msweather/data/ui/activities/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         //Anko provides a very easy DSL to deal with asynchronous, which fits most basic needs.
         val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
                 "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
-
         doAsync {
             Request(url).run()
             uiThread { longToast("Request performed") }
