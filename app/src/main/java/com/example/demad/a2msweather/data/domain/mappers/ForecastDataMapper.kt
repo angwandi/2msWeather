a22ff5e0c98df091edb72a92745ce39249b146dc
Innocent Angwadia <demsdema217@gmail.com>
2018-11-19 21:30:29 +0000

    add onclick listener

--- app/src/main/java/com/example/demad/a2msweather/data/domain/mappers/ForecastDataMapper.kt
+++ app/src/main/java/com/example/demad/a2msweather/data/domain/mappers/ForecastDataMapper.kt
@@ -11,11 +11,7 @@ import com.example.demad.a2msweather.data.domain.model.Forecast as ModelForecast
 class ForecastDataMapper {
 
     fun convertFromDataModel(forecast: ForecastResult): ForecastList =
-        ForecastList(
-            forecast.city.name,
-            forecast.city.country,
-            convertForecastListToDomain(forecast.list)
-        )
+        ForecastList(forecast.city.name, forecast.city.country, convertForecastListToDomain(forecast.list))
 
     private fun convertForecastListToDomain(list: List<Forecast>): List<ModelForecast> {
         return list.mapIndexed { i, forecast ->
@@ -25,14 +21,14 @@ class ForecastDataMapper {
     }
 
     private fun convertForecastItemToDomain(forecast: Forecast): ModelForecast {
-        return ModelForecast(
-            convertDate(forecast.dt), forecast.weather[0].description,
-            forecast.temp.max.toInt(), forecast.temp.min.toInt()
-        )
+        return ModelForecast(convertDate(forecast.dt), forecast.weather[0].description,
+            forecast.temp.max.toInt(), forecast.temp.min.toInt(), generateIconUrl(forecast.weather[0].icon))
     }
 
     private fun convertDate(date: Long): String {
         val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
         return df.format(date)
     }
+
+    private fun generateIconUrl(iconCode: String): String = "http://openweathermap.org/img/w/$iconCode.png"
 }
---
+++ app/src/main/java/com/example/demad/a2msweather/data/utils/ViewExtensions.kt
@@ -0,0 +1,7 @@
+package com.example.demad.a2msweather.data.utils
+
+import android.content.Context
+import android.view.View
+
+val View.ctx: Context
+    get() = context
